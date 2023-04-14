505aa679ae4d155c03db7791d716c54209ced1e4
Ming <chiangming07@gmail.com>
2023-04-14 13:22:13 +0800

    [Add] abstract route

--- server/routes/chat_route.js
+++ server/routes/chat_route.js
@@ -2,7 +2,9 @@ import express from "express";
 const router = express.Router();
 
 import { getRoomId } from "../controllers/chatroom_controller.js";
+import { sendAbstract } from "../controllers/mailgun_controller.js";
 
 router.route("/chat/roomId").post(getRoomId);
+router.route("/chat/abstract").post(sendAbstract);
 
 export { router };

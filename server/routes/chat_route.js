ed3c89914a791a95858fd2694f78a8c285511e3a
Ming <chiangming07@gmail.com>
2023-04-18 21:18:29 +0800

    [Add] searchHistory controller

--- server/routes/chat_route.js
+++ server/routes/chat_route.js
@@ -4,9 +4,11 @@ const router = express.Router();
 import { getRoomId } from "../controllers/chatroom_controller.js";
 import { sendAbstract } from "../controllers/mailgun_controller.js";
 import { getHistory } from "../controllers/chatHistory_controller.js";
+import { searchHistory } from "../controllers/searchHistory_controller.js";
 
 router.route("/chat/roomId").post(getRoomId);
 router.route("/chat/abstract").post(sendAbstract);
 router.route("/chat/history").get(getHistory);
+router.route("/chat/searchHistory").get(searchHistory);
 
 export { router };

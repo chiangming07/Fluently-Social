db04c97ba3ea916955d27f22f7fc495f7e9519f1
Ming <chiangming07@gmail.com>
2023-05-03 18:28:48 +0800

    [Update] improve UI by showing current status

--- client/src/pages/User/Profile/Profile.jsx
+++ client/src/pages/User/Profile/Profile.jsx
@@ -1,22 +1,12 @@
-// import React, { useState } from "react";
-// import styled from "styled-components";
-// import {
-//   Container,
-//   Avatar,
-//   Info,
-//   Field,
-//   ProfileLabel,
-//   ProfileInput,
-//   Select,
-//   GenderSelect,
-//   GenderLabel,
-//   GenderOption,
-//   Button,
-// } from "../Style";
-
 import { useState, useEffect } from "react";
 import { useNavigate } from "react-router-dom";
-import styled from "styled-components";
+import { useRecoilState } from "recoil";
+import { isLoggedInAtom } from "../../../recoil/atoms";
+
+import { ToastContainer, toast } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
+
+import styled from "styled-components/macro";
 
 import MyInfo from "./components/MyInfo";
 import MyPreference from "./components/MyPreference";
@@ -52,6 +42,8 @@ const TabButton = styled.button`
   box-shadow: 0 0 5px rgba(0, 0, 0, 0.2);
   color: ${({ active }) => (active ? "#5b9f59" : "#666")};
   font-weight: ${({ active }) => (active ? "bold" : "normal")};
+  box-shadow: ${({ active }) =>
+    active ? "0 0 5px rgba(0, 0, 0, 0.2), inset 5px 0 #7cc096" : ""};
   /* font-size: ${({ active }) => (active ? "16px" : "14px")}; */
   font-size: 16px;
   white-space: nowrap;
@@ -61,7 +53,7 @@ const TabButton = styled.button`
 
   &:hover {
     color: #5b9f59ce;
-    box-shadow: 0 0 5px rgba(0, 0, 0, 0.2), inset 5px 0 #7cc096;
+    /* box-shadow: 0 0 5px rgba(0, 0, 0, 0.2), inset 5px 0 #7cc096; */
   }
 `;
 
@@ -70,38 +62,70 @@ const LogoutButton = styled(TabButton)`
   position: absolute;
   width: 80%;
   bottom: 10px;
+  &:hover {
+    color: #e96734ce;
+    box-shadow: 0 0 5px rgba(0, 0, 0, 0.2), inset 5px 0 #e96734ce;
+  }
 `;
 
 const Profile = () => {
   const navigate = useNavigate();
+  const [isLoggedIn, setIsLoggedIn] = useRecoilState(isLoggedInAtom);
   const [activeTab, setActiveTab] = useState("info");
+  const [avatar, setAvatar] = useState("");
   const [username, setUsername] = useState("");
   const [email, setEmail] = useState("");
   const [gender, setGender] = useState("");
   const [age, setAge] = useState("");
+  const [speaking, setSpeaking] = useState([]);
+  const [learning, setLearning] = useState([]);
+  const [selectedTopic, setSelectedTopic] = useState([]);
+
+  const errorNotify = (msg) => {
+    toast.error(msg, {
+      position: "top-center",
+      autoClose: 1500,
+      onClose: () => {
+        navigate("/login");
+      },
+    });
+  };
 
   useEffect(() => {
     const accessToken = localStorage.getItem("accessToken");
+
     if (!accessToken) {
-      alert("Please log in to continue.");
-      navigate("/login");
+      errorNotify("Please log in to continue.");
       return;
     }
+
     const renderProfile = async (accessToken) => {
       try {
         const response = await api.getProfile(accessToken);
         if (response.status === 200) {
           const user = JSON.parse(localStorage.getItem("user"));
-          const { username, email, gender, age } = user;
+          const {
+            avatar,
+            username,
+            email,
+            gender,
+            age,
+            speaking,
+            learning,
+            topic,
+          } = user;
+          setAvatar(avatar);
           setUsername(username);
           setEmail(email);
           setGender(gender);
           setAge(age);
+          setSpeaking(speaking);
+          setLearning(learning);
+          setSelectedTopic(topic);
         }
       } catch (e) {
         console.log(e.response.data.message);
-        alert("Connection timed out. Please log in again.");
-        navigate("/login");
+        errorNotify("Connection timed out. Please log in again.");
         return;
       }
     };
@@ -112,8 +136,15 @@ const Profile = () => {
     setActiveTab(tab);
   };
 
+  const handleLogOut = () => {
+    localStorage.clear();
+    setIsLoggedIn(false);
+    navigate("/login");
+    return;
+  };
   return (
     <ProfileContainer>
+      <ToastContainer />
       <TabContainer>
         <TabButton
           active={activeTab === "info"}
@@ -127,12 +158,27 @@ const Profile = () => {
         >
           My Preference
         </TabButton>
-        <LogoutButton>Log out</LogoutButton>
+        <LogoutButton onClick={() => handleLogOut()}>Log out</LogoutButton>
       </TabContainer>
       {activeTab === "info" && (
-        <MyInfo username={username} email={email} gender={gender} age={age} />
+        <MyInfo
+          avatar={avatar}
+          username={username}
+          email={email}
+          gender={gender}
+          age={age}
+        />
       )}
-      {activeTab === "preference" && <MyPreference />}
+      {activeTab === "preference" && (
+        <MyPreference
+          speaking={speaking}
+          setSpeaking={setSpeaking}
+          learning={learning}
+          setLearning={setLearning}
+          selectedTopic={selectedTopic}
+          setSelectedTopic={setSelectedTopic}
+        />
+      )}
     </ProfileContainer>
   );
 };

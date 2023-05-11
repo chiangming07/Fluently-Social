0ba43a815f5336a02ff5637e332772e0874cf420
Ming <chiangming07@gmail.com>
2023-05-11 17:00:48 +0800

    [Add] upload avatar

--- client/src/pages/User/Profile/Profile.jsx
+++ client/src/pages/User/Profile/Profile.jsx
@@ -18,49 +18,33 @@ const ProfileContainer = styled.div`
   align-items: center;
   margin: 0 auto;
   text-align: center;
-  width: 80%;
+  width: 85%;
   margin-top: 150px;
 `;
 
-const TabContainer = styled.div`
+const InfoContainer = styled.div`
   display: flex;
   flex-direction: column;
   position: relative;
-  width: 20%;
+  width: 30%;
   padding-right: 40px;
-  border-right: 3px solid rgba(0, 0, 0, 0.2);
+  border-right: 2px solid rgba(0, 0, 0, 0.2);
   height: 79vh;
 `;
 
-const TabButton = styled.button`
+const LogoutButton = styled.button`
+  position: absolute;
+  bottom: 10px;
+  width: calc(100% - 40px);
   padding: 15px 30px;
-  margin-bottom: 10px;
   border: none;
-  border-radius: 10px;
+  border-radius: 8px;
   background-color: transparent;
   box-shadow: 0 0 5px rgba(0, 0, 0, 0.2);
-  color: ${({ active }) => (active ? "#5b9f59" : "#666")};
-  font-weight: ${({ active }) => (active ? "bold" : "normal")};
-  box-shadow: ${({ active }) =>
-    active ? "0 0 5px rgba(0, 0, 0, 0.2), inset 5px 0 #7cc096" : ""};
-  /* font-size: ${({ active }) => (active ? "16px" : "14px")}; */
   font-size: 16px;
-  white-space: nowrap;
-
-  cursor: pointer;
   transition: all 0.2s ease-in-out;
+  cursor: pointer;
 
-  &:hover {
-    color: #5b9f59ce;
-    /* box-shadow: 0 0 5px rgba(0, 0, 0, 0.2), inset 5px 0 #7cc096; */
-  }
-`;
-
-const LogoutButton = styled(TabButton)`
-  margin-bottom: 0px;
-  position: absolute;
-  width: 80%;
-  bottom: 10px;
   &:hover {
     color: #e96734ce;
     box-shadow: 0 0 5px rgba(0, 0, 0, 0.2), inset 5px 0 #e96734ce;
@@ -70,12 +54,9 @@ const LogoutButton = styled(TabButton)`
 const Profile = () => {
   const navigate = useNavigate();
   const [isLoggedIn, setIsLoggedIn] = useRecoilState(isLoggedInAtom);
-  const [activeTab, setActiveTab] = useState("info");
   const [avatar, setAvatar] = useState("");
   const [username, setUsername] = useState("");
   const [email, setEmail] = useState("");
-  const [gender, setGender] = useState("");
-  const [age, setAge] = useState("");
   const [speaking, setSpeaking] = useState([]);
   const [learning, setLearning] = useState([]);
   const [selectedTopic, setSelectedTopic] = useState([]);
@@ -106,21 +87,10 @@ const Profile = () => {
         const response = await api.getProfile(accessToken);
         if (response.status === 200) {
           const user = JSON.parse(localStorage.getItem("user"));
-          const {
-            avatar,
-            username,
-            email,
-            gender,
-            age,
-            speaking,
-            learning,
-            topic,
-          } = user;
+          const { avatar, username, email, speaking, learning, topic } = user;
           setAvatar(avatar);
           setUsername(username);
           setEmail(email);
-          setGender(gender);
-          setAge(age);
           setSpeaking(speaking);
           setLearning(learning);
           setSelectedTopic(topic);
@@ -134,10 +104,6 @@ const Profile = () => {
     renderProfile(accessToken);
   }, []);
 
-  const handleTabClick = (tab) => {
-    setActiveTab(tab);
-  };
-
   const handleLogOut = () => {
     localStorage.clear();
     setIsLoggedIn(false);
@@ -146,40 +112,23 @@ const Profile = () => {
   };
   return (
     <ProfileContainer>
-      <TabContainer>
-        <TabButton
-          active={activeTab === "info"}
-          onClick={() => handleTabClick("info")}
-        >
-          My Info
-        </TabButton>
-        <TabButton
-          active={activeTab === "preference"}
-          onClick={() => handleTabClick("preference")}
-        >
-          My Preference
-        </TabButton>
-        <LogoutButton onClick={() => handleLogOut()}>Log out</LogoutButton>
-      </TabContainer>
-      {activeTab === "info" && (
+      <InfoContainer>
         <MyInfo
           avatar={avatar}
+          setAvatar={setAvatar}
           username={username}
           email={email}
-          gender={gender}
-          age={age}
         />
-      )}
-      {activeTab === "preference" && (
-        <MyPreference
-          speaking={speaking}
-          setSpeaking={setSpeaking}
-          learning={learning}
-          setLearning={setLearning}
-          selectedTopic={selectedTopic}
-          setSelectedTopic={setSelectedTopic}
-        />
-      )}
+        <LogoutButton onClick={() => handleLogOut()}>Log out</LogoutButton>
+      </InfoContainer>
+      <MyPreference
+        speaking={speaking}
+        setSpeaking={setSpeaking}
+        learning={learning}
+        setLearning={setLearning}
+        selectedTopic={selectedTopic}
+        setSelectedTopic={setSelectedTopic}
+      />
     </ProfileContainer>
   );
 };

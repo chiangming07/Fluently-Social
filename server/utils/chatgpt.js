7bd1681fbbdd838f3a581fe050641f89a9280d7f
Ming <chiangming07@gmail.com>
2023-05-20 16:37:36 +0800

    [Update] set domain for sending email and correct time

--- server/utils/chatgpt.js
+++ server/utils/chatgpt.js
@@ -1,6 +1,7 @@
-import { Configuration, OpenAIApi } from "openai";
 import dotenv from "dotenv";
 dotenv.config();
+
+import { Configuration, OpenAIApi } from "openai";
 
 const configuration = new Configuration({
   apiKey: process.env.OPENAI_API_KEY,
@@ -8,11 +9,11 @@ const configuration = new Configuration({
 const openai = new OpenAIApi(configuration);
 
 // --- model: "gpt-3.5-turbo" ---
-const getAbstract = async (conversation, senderId) => {
+const getAbstract = async (conversation, username) => {
   const messages = [
     {
       role: "user",
-      content: `請站在 ${senderId} 的立場，把以下對話內容進行中英文摘要，此外，你需要條列出文中最困難的10個單字及可能與主題相關的5個額外單字或片語，每個單字要用1~10依序條列(包含將中文及另一語言翻譯成中文的對照表，如：面白い：有趣)。
+      content: `請站在 ${username} 的立場，把以下對話內容進行中英文摘要，此外，你需要條列出文中最困難的10個單字及可能與主題相關的5個額外單字或片語，每個單字要用1~10依序條列(包含將中文及另一語言翻譯成中文的對照表，如：面白い：有趣)。
       格式請用\n\n🌱 本日摘要：...\n\n🌱 本日單字：...\n\n🌱 額外學習：...:\n\n${conversation}"`,
     },
   ];
